e9a27b861aaa6649a69d26750c331661d7bfcc82
rishikeshrokade <rishikesh@caphillbrands.com>
2024-04-15 16:48:48 +0530

    first commit

---
+++ src/components/Body.tsx
@@ -0,0 +1,10 @@
+export const Body = () => {
+    return (
+        <div>
+            <img
+                className="w-screen "
+                src="https://assets.nflxext.com/ffe/siteui/vlv3/c1366fb4-3292-4428-9639-b73f25539794/3417bf9a-0323-4480-84ee-e1cb2ff0966b/IN-en-20240408-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+            />
+        </div>
+    );
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <div>
+            <div className="absolute text-4xl font-semibold m-2"> Netflix</div>
+        </div>
+    );
+};
